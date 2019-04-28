b81bb10b21ba2a5043d395b923347561c5608f5b
Elvyria <valerisafi@gmail.com>
2019-04-28 18:04:20 +0300

    DeviantArt support

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,9 @@
-var ribbon = document.getElementById("reddit-ribbon");
+var ribbon = document.querySelector(".reddit-ribbon");
+let observer = lozad(".reddit-post > img", {
+	loaded: function(elem) {
+		elem.removeAttribute("data-src");
+	}
+});
 
 function addPosts(posts) {
 	posts.forEach((item, i) => addToRibbon(item.data));
@@ -10,10 +15,12 @@ function addToRibbon(post) {
 	div.className = "reddit-post";
 
 	let h2 = document.createElement("h2");
-	// Such posts require innerHTML instead of textContent (reddit.com/r/awwnime/comments/bhew2y/the_rainy_season_hero_froppy_3_boku_no_hero/)
+	// Such posts require innerHTML instead of textContent
+	// (reddit.com/r/awwnime/comments/bhew2y/the_rainy_season_hero_froppy_3_boku_no_hero/)
 	h2.innerText = post.title;
 	div.appendChild(h2);
 
+	// TODO: Simplify
 	switch(post.post_hint) {
 		case "image":
 			let img = document.createElement("img");
@@ -24,14 +31,19 @@ function addToRibbon(post) {
 			div.innerHTML += decodeHTML(post.media.oembed.html);
 			break;
 		case "link":
-			if (post.url.includes("imgur.com/a/")) {
-				div.appendChild(imgur(post.url));
+			// TODO: Extract to prevent switch ladders.
+			switch (post.domain) {
+				case "imgur.com":
+					div.appendChild(imgur(post.url));
+					break;
+				case "deviantart.com":
+					div.appendChild(deviantart(post.url));
+					break;
 			}
 			break;
 		default:
 			let textNode = document.createElement("div");
 			div.innerHTML += decodeHTML(post.selftext_html);
-
 	}
 
 	ribbon.appendChild(div);
@@ -53,22 +65,32 @@ function imgur(url) {
 	return frame;
 }
 
+function deviantart(url) {
+	let backendURL = "https://backend.deviantart.com/oembed?format=jsonp&callback=?&url=";
+	let img = document.createElement("img");
+	// TODO: Handle not json responses.
+	// Why we can't do it without jquery or any 3rd party lib
+	// https://stackoverflow.com/questions/43824511/fetch-error-no-access-control-allow-origin-header-is-present-on-the-requested
+	$.getJSON(backendURL + url, json => { img.setAttribute("data-src", json.url); img.setAttribute("data-loaded", false); observer.observe(); } );
+	return img;
+}
+
 function loadMore(subreddit, after = "", limit = "") {
 	let url = "http://www.reddit.com" + subreddit + ".json" + "?after=" + after + "&limit=" + limit;
+	// TODO: Handle not json responses.
 	fetch(url)
 		.then(resp => resp.json())
-		.then(resp => {
-			addPosts(resp.data.children)
-			let images = document.querySelectorAll(".reddit-post > img");
-			let observer = lozad(images);
+		.then(json => {
+			addPosts(json.data.children)
 			observer.observe();
 		});
 }
 
 loadMore("/r/dfo/", ribbon.lastChild.id);
 
+// TODO: Should start loading not at the bottom, but somewhere close to it.
 window.onscroll = function() {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-	    loadMore("/r/awwnime/", ribbon.lastChild.id);
+	    loadMore("/r/dfo/", ribbon.lastChild.id);
     }
 };
