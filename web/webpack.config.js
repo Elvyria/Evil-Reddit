3627a08b5bea2fb7572a6336c2857c63955b369b
GreedyEnder <art_saif@mail.ru>
2019-05-08 21:01:16 +0300

    Changed webpack export mode to development

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	entry: './js/src/reddit.js',
-	mode: 'production',
+	mode: 'development',
 	output: {
 		filename: 'reddit.bundle.js',
 		path: path.resolve(__dirname, 'js')
