bb33e7f3cc6e6f3f9593571e72662cf4a3d54975
Elvyria <elvyria@tutanota.com>
2021-03-16 20:41:17 +0300

    Realization of basic hotkeys

--- web/js/src/hotkeys.js
+++ web/js/src/hotkeys.js
@@ -1,8 +1,41 @@
-export function hotkeys() {
-	// document.addEventListener("keydown", e => {
-		// switch (e.code) {
-			// case "Key?": {
-			// }
-		// }
-	// })
+import { hide, show } from "./globals.js"
+
+const searchInput = document.querySelector("input")
+
+const keys = {
+	"/": { 
+		"action": (event) => {
+			if (document.activeElement !== searchInput) {
+				searchInput.focus()
+				event.preventDefault()
+			}
+		},
+		"description": "Focus search box"
+	},
+	"Enter": {
+		"action": (event) => {
+			if (document.activeElement === searchInput)
+				searchInput.blur()
+		},
+	},
+	"Escape": {
+		"name": "Esc",
+		"action": (event) => {
+			if (document.activeElement === searchInput)
+				searchInput.blur()
+		},
+	},
+	"?": {
+		"action": () => {
+			// Something something show(help)
+		},
+		"description": "Show help window"
+	}
 }
+
+document.addEventListener("keydown", e => {
+	const hotkey = keys[e.key]
+
+	if (hotkey)
+		hotkey.action(e)
+})
