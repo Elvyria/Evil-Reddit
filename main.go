448924bc780a0d793281a540ebef0ff3da11356c
Elvyria <elvyria@tutanota.com>
2021-03-19 12:01:17 +0300

    Gentle server shutdown

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	var port uint64 = 3006
+	var err error
+
+	args := os.Args[1:]
+
+	if len(args) != 0 {
+		port, err = strconv.ParseUint(args[0], 10, 0)
+		if err != nil || port < 2 {
+			fmt.Println("Invalid port specified")
+
+			os.Exit(1)
+		}
+	}
+
+	address := ":" + strconv.FormatUint(port, 10)
+
 	e := echo.New()
+	e.HideBanner = true
 	e.Static("/", "web")
 	e.GET("/r/*", getSubreddit)
-	e.Logger.Fatal(e.Start(":3006"))
+
+	go func() {
+		e.Logger.Fatal(e.Start(address))
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	if err = e.Shutdown(context.Background()); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
 
 func getSubreddit(c echo.Context) error {
