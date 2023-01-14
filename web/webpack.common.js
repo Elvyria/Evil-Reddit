6271b804b998379df82b4e45191843bebfa6afe9
Elvyria <elvyria@tutanota.com>
2023-01-14 08:41:02 +0300

    Upgraded dependencies, removed Babel

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -2,18 +2,6 @@ const path = require('path');
 
 module.exports = {
 	entry: './js/src/reddit.js',
-	module: {
-		rules: [{
-			test: /\.m?js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-				}
-			}
-		}]
-	},
 	output: {
 		filename: 'reddit.js',
 		path: path.resolve(__dirname, 'js')
